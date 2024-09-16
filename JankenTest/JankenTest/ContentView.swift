4703c31e0023cfa7dbdf87f5d0747cac8994796d
T.Kiriu <144713861+Kiriu0322@users.noreply.github.com>
2024-09-16 21:18:41 +0900

    New Project
    ローカルで作成していたじゃんけんアプリプロジェクトのアップロード

---
+++ JankenTest/JankenTest/ContentView.swift
@@ -0,0 +1,103 @@
+//
+//  ContentView.swift
+//  JankenTest
+//
+//  Created by Kiriu Tomoki on 2024/09/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var jankenNumber = 0
+    let gradient = LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+
+    var body: some View {
+        ZStack{
+            Circle()
+                .fill(gradient)
+                .opacity(0.2)
+                .position(.init(x: 50, y: 50))
+            Circle()
+                .fill(gradient)
+                .opacity(0.2)
+                .position(.init(x: 300, y: 500))
+            VStack {
+                Spacer()
+                if jankenNumber == 0 {
+                    Text("これからじゃんけんをします！")
+                        .padding(.bottom)
+                }else if jankenNumber == 1{
+                    Image(.gu)
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                    Spacer()
+                    Text("ぐー")
+                        .padding(.bottom)
+                }else if jankenNumber == 2{
+                    Image(.choki)
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.yellow)
+                    Spacer()
+                    Text("ちょき")
+                        .padding(.bottom)
+                }else{
+                    Image(.pa)
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.pink)
+                    Spacer()
+                    Text("ぱー")
+                        .padding(.bottom)
+                }
+                
+                HStack{
+                    Button(action: {
+                        jankenNumber = Int.random(in: 1...3)
+                    }, label: {
+                        Text("ぐー")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 100)
+                            .font(.title)
+                            .background(.blue)
+                            .foregroundColor(.black)
+                        
+                    })
+                    Button(action: {
+                        jankenNumber = Int.random(in: 1...3)
+                    }, label: {
+                        Text("ちょき")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 100)
+                            .font(.title)
+                            .background(.yellow)
+                            .foregroundColor(.black)
+                        
+                    })
+                    Button(action: {
+                        jankenNumber = Int.random(in: 1...3)
+                    }, label: {
+                        Text("ぱー")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 100)
+                            .font(.title)
+                            .background(.pink)
+                            .foregroundColor(.black)
+                        
+                    })
+                }
+                
+            }
+        }
+
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JankenTest/JankenTest/JankenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenTestApp.swift
+//  JankenTest
+//
+//  Created by Kiriu Tomoki on 2024/09/12.
+//
+
+import SwiftUI
+
+@main
+struct JankenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
